249b6d53d5612e51349e3dcba689121f4b781a22
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-06-28 14:21:34 +0200

    Formatted code with prettier

--- spa_xhr_navigation.js
+++ spa_xhr_navigation.js
@@ -1,111 +1,112 @@
-(function() {
-	BOOMR = window.BOOMR || {};
-	BOOMR.plugins = BOOMR.plugins || {};
+(function () {
+  BOOMR = window.BOOMR || {};
+  BOOMR.plugins = BOOMR.plugins || {};
 
-	if (BOOMR.plugins.spa_xhr_navigation) {
-		return;
-	}
+  if (BOOMR.plugins.spa_xhr_navigation) {
+    return;
+  }
 
-	var impl = {
-		initialized: false,
-		navigationPath: function(){return null;},
-		disableHardNav: false,
-		a: null, //an anchor element to resolve paths
-		onXhrSend: function(xhr_info) {
-			var resultPath = this.navigationPath(xhr_info);
-			if(resultPath) {
-				this.a.href = resultPath;
-				var resolvedPath = this.a.href;
-				BOOMR.plugins.SPA.route_change()
-				BOOMR.addVar("pgu",resolvedPath)
-			}
-		}
-	}
+  var impl = {
+    initialized: false,
+    navigationPath: function () {
+      return null;
+    },
+    disableHardNav: false,
+    a: null, //an anchor element to resolve paths
+    onXhrSend: function (xhr_info) {
+      var resultPath = this.navigationPath(xhr_info);
+      if (resultPath) {
+        this.a.href = resultPath;
+        var resolvedPath = this.a.href;
+        BOOMR.plugins.SPA.route_change();
+        BOOMR.addVar("pgu", resolvedPath);
+      }
+    },
+  };
 
-	// Instrumentation based on the AutoXHR Plugin
-	var initInstrumentation = function() {
+  // Instrumentation based on the AutoXHR Plugin
+  var initInstrumentation = function () {
+    var original_XHR = XMLHttpRequest;
+    var instr_XMLHttpRequest = function () {
+      var xhr_info = {};
+      var xhr_header_calls = [];
+      var open_args;
 
-		var original_XHR = XMLHttpRequest;
-		var instr_XMLHttpRequest = function() {
+      req = new original_XHR();
 
-			var xhr_info = {};
-			var xhr_header_calls = [];
-			var open_args;
+      orig_open = req.open;
+      orig_setRequestHeader = req.setRequestHeader;
+      orig_send = req.send;
 
-			req = new original_XHR();
+      req.setRequestHeader = function () {
+        xhr_header_calls.push(arguments);
+      };
 
-			orig_open = req.open;
-			orig_setRequestHeader = req.setRequestHeader;
-			orig_send = req.send;
+      req.open = function (method, url, async) {
+        xhr_info.method = method;
+        xhr_info.url = url;
+        xhr_info.async = !!async;
+        open_args = arguments;
+        //we delay the open-call to make sure it happens after the navigation
+      };
 
-			
-			req.setRequestHeader = function () {
-				xhr_header_calls.push(arguments);	
-			};
+      req.send = function (data) {
+        xhr_info.body = data;
+        impl.onXhrSend(xhr_info);
+        orig_open.apply(this, open_args);
+        for (var i = 0; i < xhr_header_calls.length; i++) {
+          orig_setRequestHeader.apply(this, xhr_header_calls[i]);
+        }
+        orig_send.apply(this, arguments);
+      };
 
-			req.open = function(method, url, async) {
-				xhr_info.method = method;
-				xhr_info.url = url;
-				xhr_info.async = !!async;
-				open_args = arguments;
-				//we delay the open-call to make sure it happens after the navigation
-			};
+      return req;
+    };
 
-			req.send = function(data) {
-				xhr_info.body = data;
-				impl.onXhrSend(xhr_info);
-				orig_open.apply(this, open_args);
-				for (var i = 0; i < xhr_header_calls.length; i++) {
-					orig_setRequestHeader.apply(this, xhr_header_calls[i]);
-				}
-				orig_send.apply(this, arguments);
-			};
+    instr_XMLHttpRequest.UNSENT = 0;
+    instr_XMLHttpRequest.OPENED = 1;
+    instr_XMLHttpRequest.HEADERS_RECEIVED = 2;
+    instr_XMLHttpRequest.LOADING = 3;
+    instr_XMLHttpRequest.DONE = 4;
+    instr_XMLHttpRequest.prototype = XMLHttpRequest.prototype;
+    window.XMLHttpRequest = instr_XMLHttpRequest;
+  };
 
-			return req;
-		};
+  BOOMR.plugins.spa_xhr_navigation = {
+    /**
+     * Initializes the plugin.
+     *
+     * @param {object} config Configuration
+     * @param {function} [config.spa_xhr_navigation.navigationPath]
+     *
+     * @returns {@link BOOMR.plugins.spa_xhr_navigation} The spa_xhr_navigation plugin for chaining
+     * @memberof BOOMR.plugins.spa_xhr_navigation
+     */
+    init: function (config) {
+      if (impl.initialized) {
+        return this;
+      }
 
-		instr_XMLHttpRequest.UNSENT = 0;
-		instr_XMLHttpRequest.OPENED = 1;
-		instr_XMLHttpRequest.HEADERS_RECEIVED = 2;
-		instr_XMLHttpRequest.LOADING = 3;
-		instr_XMLHttpRequest.DONE = 4;
-		instr_XMLHttpRequest.prototype = XMLHttpRequest.prototype;
-		window.XMLHttpRequest = instr_XMLHttpRequest;
-	}
+      BOOMR.utils.pluginConfig(impl, config, "spa_xhr_navigation", [
+        "navigationPath",
+        "disableHardNav",
+      ]);
 
-	BOOMR.plugins.spa_xhr_navigation = {
-		/**
-		 * Initializes the plugin.
-		 *
-		 * @param {object} config Configuration
-		 * @param {function} [config.spa_xhr_navigation.navigationPath] 
-		 * 
-		 * @returns {@link BOOMR.plugins.spa_xhr_navigation} The spa_xhr_navigation plugin for chaining
-		 * @memberof BOOMR.plugins.spa_xhr_navigation
-		 */
-		init: function(config) {
-			if(impl.initialized) {
-				return this;
-			}
+      impl.a = document.createElement("a"); //used to resolve paths to URLs
+      initInstrumentation();
 
-			BOOMR.utils.pluginConfig(impl, config, "spa_xhr_navigation", ["navigationPath","disableHardNav"]);
+      BOOMR.plugins.SPA.register("XHR_NAVIGATION");
+      BOOMR.plugins.SPA.hook(BOOMR.hasBrowserOnloadFired(), {});
 
-			impl.a = document.createElement("a"); //used to resolve paths to URLs
-			initInstrumentation();
-			
-			BOOMR.plugins.SPA.register("XHR_NAVIGATION");
-			BOOMR.plugins.SPA.hook(BOOMR.hasBrowserOnloadFired(), {});
+      if (!BOOMR.hasBrowserOnloadFired() && !impl.disableHardNav) {
+        BOOMR.plugins.SPA.route_change();
+      }
 
-			if (!BOOMR.hasBrowserOnloadFired() && !impl.disableHardNav) {
-				BOOMR.plugins.SPA.route_change();
-			}
+      return this;
+    },
 
-			return this;
-		},
-
-		is_complete: function() {
-			return true;
-		}
-	};
-
-}());
+    is_complete: function () {
+      return true;
+    },
+  };
+})();
